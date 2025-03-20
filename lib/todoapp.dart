13a60b37601ba3c894ad4c11e8a9b40479c7385d
nauvalachid <nauvalachid@gmail.com>
2025-03-20 22:45:32 +0700

    deklarasi statefulwidget

--- lib/todoapp.dart
+++ lib/todoapp.dart
@@ -1,2 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({super.key});
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
